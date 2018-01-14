93a9f751e677cb6e5efe1f82cc4aa2f23069c469
salosek <salosek@nppd.com>
2018-01-14 12:24:31 -0600

    final 20180114 1224

--- plot4.R
+++ plot4.R
@@ -39,9 +39,9 @@ plot(x$DateTime, x$sub1, pch = "", lty = "solid", col = "black", ylab = "Energy 
 lines(x$DateTime, x$sub1)
 lines(x$DateTime, x$sub2, col = "red")
 lines(x$DateTime, x$sub3, col = "blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = "solid")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = "solid", bty = "n")
 
-plot(x$DateTime, x$GRP, pch = "", lty = "solid", col = "black", ylab = "Global_reative_power", xlab = "datetime")
+plot(x$DateTime, x$GRP, pch = "", lty = "solid", col = "black", ylab = "Global_reactive_power", xlab = "datetime")
 lines(x$DateTime, x$GRP)
 
 
@@ -59,9 +59,9 @@ plot(x$DateTime, x$sub1, pch = "", lty = "solid", col = "black", ylab = "Energy 
 lines(x$DateTime, x$sub1)
 lines(x$DateTime, x$sub2, col = "red")
 lines(x$DateTime, x$sub3, col = "blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = "solid")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = "solid", bty = "n")
 
-plot(x$DateTime, x$GRP, pch = "", lty = "solid", col = "black", ylab = "Global_reative_power", xlab = "datetime")
+plot(x$DateTime, x$GRP, pch = "", lty = "solid", col = "black", ylab = "Global_reactive_power", xlab = "datetime")
 lines(x$DateTime, x$GRP)
 
 
